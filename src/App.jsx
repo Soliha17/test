d0fbb332abc91c72e06fa9f79ed9e73e23a4a5d4
Soliha <solyatursuntasheva@gmail.com>
2023-07-25 21:36:29 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container h-screen w-screen bg-red-400 text-center">
+      <h1 className="text-5xl font-bold underline hover:text-blue-600">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
